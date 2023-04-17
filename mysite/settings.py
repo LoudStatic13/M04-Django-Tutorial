e98ce9182dc778f427eff48063657baed4095296
LoudStatic13 <125827390+LoudStatic13@users.noreply.github.com>
2023-04-17 17:53:40 -0400

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dnf)=vd7#-nh3rgx_7h5-!b$#p45kfyw27dg9ae5vmydt3io*m'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
